72dce22fe40ecbbcea3f2b4ed13087312d221374
Vijay <vijaypatil15081989@gmail.com>
2015-03-30 01:01:38 -0400

    Change in Method LordVetinari

--- src/winningCondition/LordVetinari.java
+++ src/winningCondition/LordVetinari.java
@@ -3,6 +3,8 @@ package winningCondition;
 import java.util.ArrayList;
 
 import ankhmorpork.Game.Game;
+import ankhmorpork.GameObjects.Building;
+import ankhmorpork.GameObjects.Minion;
 import ankhmorpork.GameObjects.Player;
 import ankhmorpork.GameObjects.Cards.CityAreaCard;
 
@@ -11,17 +13,40 @@ public class LordVetinari implements WinningCondition {
 	@Override
     public Boolean isWinner(int PlayerID) {
 		int numberOfPlayers = Game.lstPlayers.size();
-		ArrayList<CityAreaCard> lstCityAreaCard = Game.GetCityAreaCardByPlayerID(PlayerID);
-		if (numberOfPlayers == 2 && lstCityAreaCard.size() == 11)
+		Player objPlayer1 = Game.GetPlayer(PlayerID);
+//		ArrayList<CityAreaCard> lstCityAreaCard = Game.GetCityAreaCardByPlayerID(PlayerID);
+		int noOfcontrolledArea = ControlledArea(objPlayer1);
+		if (numberOfPlayers == 2 && noOfcontrolledArea == 11)
            return Boolean.TRUE;
-		else if (numberOfPlayers == 3 && lstCityAreaCard.size() == 10)
+		else if (numberOfPlayers == 3 && noOfcontrolledArea == 10)
            return Boolean.TRUE;
-		else if (numberOfPlayers == 4 && lstCityAreaCard.size() == 9)
+		else if (numberOfPlayers == 4 && noOfcontrolledArea == 9)
            return Boolean.TRUE;
 		else
            return Boolean.FALSE;
 	}
+	
+	
+	private int ControlledArea(Player objPlayer)
+	{
+		//Area objArea = new Area();
+		int minionCounter = 0;
+		ArrayList<Minion> lstOfMinion = Game.GetMinionsByPlayerID(objPlayer.getPlayer_id());
+		for(Minion objMinion : lstOfMinion){
+			if(objMinion.getArea_id() != 0){
+				int areaId = objMinion.getArea_id();
+				if(!Game.AreaHasDemon(areaId))
+				{
+					minionCounter++;
+				}
+			}
+		}
+		return minionCounter;
+				
+	}
+
 }
+
 
 
 
